65ada5a4b06a2a67159d191ed73a98c75dda7e44
x <x@x.com>
2023-12-08 18:26:25 +0800

    update DNCOpenerLogic

---
+++ XIVSlothComboX/Combos/JobHelpers/DNC.cs
@@ -0,0 +1,86 @@
+﻿using Dalamud.Game.ClientState.JobGauge.Types;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using XIVSlothComboX.Combos.PvE;
+using XIVSlothComboX.Core;
+using XIVSlothComboX.CustomComboNS.Functions;
+using XIVSlothComboX.Extensions;
+
+namespace XIVSlothComboX.Combos.JobHelpers
+{
+    internal class DNCOpenerLogic : DNC
+    {    
+        
+        private static uint OpenerLevel => 90;
+        public bool DoFullOpener(ref uint actionID)
+        {
+            
+            if (!CustomComboFunctions.LevelChecked(标准舞步StandardStep))
+                return false;
+
+            if (CustomComboFunctions.InCombat())
+            {
+                return false;
+            }
+
+
+            if (!CustomComboFunctions.InCombat())
+            {
+
+                DNCGauge? gauge = CustomComboFunctions.GetJobGauge<DNCGauge>();
+                if (CustomComboFunctions.HasEffect(Buffs.标准舞步StandardStep))
+                {
+                    if (gauge.CompletedSteps < 2)
+                    {
+                        actionID = gauge.NextStep;
+                        if (actionID.ActionReady())
+                        {
+                            unsafe
+                            {
+                                ActionManager.Instance()->UseAction(ActionType.Spell, actionID);
+                            }
+                        }
+                    }
+                }
+
+
+
+                if (Countdown.TimeRemaining() <= 15)
+                {
+                    if (标准舞步StandardStep.ActionReady())
+                    {
+                        actionID = 标准舞步StandardStep;
+                        unsafe
+                        {
+                            ActionManager.Instance()->UseAction(ActionType.Spell, actionID);
+                        }
+                    }
+                }
+
+                if (Countdown.TimeRemaining() is > 0.3f and < 0.5f)
+                {
+                    if (双色标准舞步结束StandardFinish2.ActionReady())
+                    {
+                        actionID = 双色标准舞步结束StandardFinish2;
+                        unsafe
+                        {
+                            ActionManager.Instance()->UseAction(ActionType.Spell, 双色标准舞步结束StandardFinish2);
+                        }
+                    }
+                }
+
+                if (标准舞步StandardStep.ActionReady())
+                {
+                    actionID = 标准舞步StandardStep;
+                    return true;
+                }
+
+                return false;
+            }
+
+            return false;
+        }
+    }
+
+
+
+}
